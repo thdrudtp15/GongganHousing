12c64345ebbc4e3b17f97c102191ccbd31de498b
thdrudtp15 <15rudtp@naver.com>
2025-10-15 14:47:10 +0900

    feat : 사업 영역 소개 이미지 추가 및 사업영역 섹션 변경

--- components/main/Services.tsx
+++ components/main/Services.tsx
@@ -1,17 +1,9 @@
 import Section from '@/components/ui/MainSection';
-import Link from 'next/link';
-import Image from 'next/image';
+import ServiceItem from '@/components/main/ServiceItem';
 
-const ServiceItem = ({ title, desc, href }: { title: string; desc: string; href: string }) => {
-  return (
-    <article className="border border-gray-200 p-4">
-      <div className="relative"></div>
-      <h3 className="font-bold text-2xl mb-1">{title}</h3>
-      <p className="text-gray-500 mb-6">{desc}</p>
-      <Link href={href}>View More</Link>
-    </article>
-  );
-};
+import windowImage from "@/public/images/service_window.png"
+import interiorImage from "@/public/images/service_interior.webp"
+import advertising from "@/public/images/service_advertising.webp"
 
 const Services = () => {
   return (
@@ -21,21 +13,27 @@ const Services = () => {
           사업 영역
         </h2>
         <p className="text-lg text-gray-500 mb-8">OUR SERVICES</p>
-        <div className="grid grid-cols-1 md:grid-cols-3 justify-between w-full gap-4">
+        <div className='flex flex-col gap-10'>
           <ServiceItem
             title="실내 건축"
             desc="공간의 기능과 아름다움을 고려 사람의 라이프스타일과 경험을 반영 최적화된 실내 공간 설계"
             href="/portfolio?category=실내건축"
+            image={interiorImage}
+            start="left"
           />
           <ServiceItem
             title="옥외 광고물"
             desc="브랜드 가치를 돋보이게 하고 도시 속에서 강렬한 첫인상 세련되고 감각적인 옥외광고 솔루션"
             href="/portfolio?category=옥외광고물"
+            image={advertising}
+            start="right"
           />
-          <ServiceItem
+           <ServiceItem
             title="금속 창호"
             desc="견고함과 스타일을 동시에 빛과 공간을 조화롭게 담은 맞춤형 금속 창호 솔루션"
             href="/portfolio?category=금속창호"
+            image={windowImage}
+            start="left"
           />
         </div>
       </Section.Content>
